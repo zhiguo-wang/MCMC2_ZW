472fa9992ace271d28152eac38e584f5063eb08e
zhiguo-wang <jeremy.zw@aol.com>
2015-11-07 16:37:24 -0500

    modified directories

--- config.R
+++ config.R
@@ -1,38 +1,12 @@
 root <- "D:\\OneDrive\\Documents\\MCMC_2\\"
+sourceCode <- "sourceCode\\R Model\\" 
+workspaces <- "Workspaces\\"
 setwd(root)
 
 
-#setwd(paste0(root,"SourceCode"))
-
-#*********************#
-#      Constants      #
-#*********************#
-
-# 1. Tax adjustment forward from 2011 to 2014
-#    File :  MM lookup table with testing page 0918 v39
-#    Sheet: IRS lookups
-#    Cell : I10
-TaxAdj.1114 <- 1.076891
 
 
-# 1. Tax adjustment back from 2014 to 2011
-#    File :  MM lookup table with testing page 0918 v39
-#    Sheet: IRS lookups
-#    Cell : I9
-TaxAdj.1411 <- 0.928599
-
-
-# 1. Income adjustment forward from 2011 to 2014
-#    File :  MM lookup table with testing page 0918 v39
-#    Sheet: IRS lookups
-#    Cell : C10
-IncomeAdj.1114 <- 1.092727
-
-
-# 1. Income adjustment back from 2014 to 2011
-#    File :  MM lookup table with testing page 0918 v39
-#    Sheet: IRS lookups
-#    Cell : C10
-IncomeAdj.1411 <- 0.915141659
-
-
+root <- "C:\\Users\\zhiguo\\OneDrive\\Documents\\MCMC_2\\"
+sourceCode <- "sourceCode\\R Model\\" 
+workspaces <- "Workspaces\\"
+setwd(root)
